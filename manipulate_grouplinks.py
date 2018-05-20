e813851b2953946388c013b9e30d0364bd87d0ae
Philip Nye <contact@philipnye.co.uk>
2018-05-20 20:53:45 +0100

    Add basic analysis of estab type

--- manipulate_grouplinks.py
+++ manipulate_grouplinks.py
@@ -18,14 +18,21 @@ companies_house_url_stub='https://beta.companieshouse.gov.uk/company/'
 github_url="https://github.com/philipnye/ainfo/tree/master/data"
 github_raw_url="https://raw.githubusercontent.com/philipnye/ainfo/master/data/"
 
+estab_type_count={
+	'sponsored_academy_count':None,
+	'converter_academy_count':None,
+	'free_school_count':None,
+	'utc_studio_school_count':None
+}
+
 html=requests.get(github_url).text
 soup=BeautifulSoup(html, 'html.parser')
 for a in soup.find_all('a'):
-    if str(a.get('title')).endswith('csv'):     # tag.get('attr') in Beautiful Soup acts like a Python dictionary, returning None where attr is undefined.
-        file_name=str(a.get('title'))
-        file_url=github_raw_url+file_name        # expectation is that there is one and only data file
+	if str(a.get('title')).endswith('csv'):	 # tag.get('attr') in Beautiful Soup acts like a Python dictionary, returning None where attr is undefined.
+		file_name=str(a.get('title'))
+		file_url=github_raw_url+file_name		# expectation is that there is one and only data file
 csv_file=requests.get(file_url)
-csv_file=csv_file.iter_lines()      # is required in order for csv file to be read correctly, without errors caused by new-line characters
+csv_file=csv_file.iter_lines()	  # is required in order for csv file to be read correctly, without errors caused by new-line characters
 reader=csv.DictReader(csv_file)
 for row in reader:
 	if row['Group Type'] in ('Multi-academy trust','Single-academy trust'):
@@ -36,25 +43,62 @@ for row in reader:
 		trust_name=trust_name.replace('\xc9', 'E')
 		# trust_name=trust_name.decode('windows-1252')
 		# trust_name=trust_name.decode('windows-1252').encode('utf-8', 'ignore')
-		# print trust_name
 		trust_type=row['Group Type']
 		companies_house_number=row['Companies House Number']
-		if any(dict['trust_code'] == trust_code for dict in trust_list)==True:
-			for dict in trust_list:
-				if dict['trust_code']==trust_code:
-					dict['school_count']=dict['school_count']+1
+		estab_type_count=dict.fromkeys(estab_type_count, 0)
+		if row['TypeOfEstablishment (name)'] in ('Academy sponsor led','Academy alternative provision sponsor led','Academy special sponsor led','Academy 16 to 19 sponsor led'):
+			 estab_type_count['sponsored_academy_count']=1
+		elif row['TypeOfEstablishment (name)'] in ('Academy converter','Academy alternative provision converter','Academy special converter','Academy 16-19 converter'):
+			 estab_type_count['converter_academy_count']=1
+		elif row['TypeOfEstablishment (name)'] in ('Free schools','Free schools alternative provision','Free schools special','Free schools 16 to 19'):
+			 estab_type_count['free_school_count']=1
+		elif row['TypeOfEstablishment (name)'] in ('University technical college','Studio schools'):
+			 estab_type_count['utc_studio_school_count']=1
+		if row['PhaseOfEducation (name)'] in ('Primary','Middle deemed primary'):
+			 estab_phase='Primary'
+		elif row['PhaseOfEducation (name)'] in ('Secondary','Middle deemed secondary'):
+			 estab_phase='Secondary'
+		elif row['PhaseOfEducation (name)'] in ('All through'):
+			 estab_phase='All-through'
+		elif row['PhaseOfEducation (name)'] in ('Not applicable') and row['TypeOfEstablishment (name)'] in ('Academy alternative provision converter','Academy alternative provision sponsor led'):
+			 estab_phase='Alternative provision'
+		elif row['PhaseOfEducation (name)'] in ('Not applicable') and row['TypeOfEstablishment (name)'] in ('Academy special converter','Academy special sponsor led'):
+			 estab_phase='Special'
+		elif row['PhaseOfEducation (name)'] in ('16 plus'):
+			 estab_phase='16-plus'
+		if any(trust['trust_code']==trust_code for trust in trust_list)==True:
+			for trust in trust_list:
+				if trust['trust_code']==trust_code:
+					trust['school_count']=trust['school_count']+1
+					trust['estab_type_count']['sponsored_academy_count']=trust['estab_type_count']['sponsored_academy_count']+estab_type_count['sponsored_academy_count']
+					trust['estab_type_count']['converter_academy_count']=trust['estab_type_count']['converter_academy_count']+estab_type_count['converter_academy_count']
+					trust['estab_type_count']['free_school_count']=trust['estab_type_count']['free_school_count']+estab_type_count['free_school_count']
+					trust['estab_type_count']['utc_studio_school_count']=trust['estab_type_count']['utc_studio_school_count']+estab_type_count['utc_studio_school_count']
 					break
 		else:
-			trust_name_simple=re.sub('[^a-zA-Z0-9 \-\.@]', '', trust_name)
-			trust_name_simple=trust_name_simple.replace (' ', '-')
-			trust_name_simple=trust_name_simple.replace ('--', '-')
-			trust_list.append({'trust_code':trust_code, 'trust_name':trust_name, 'trust_name_simple':trust_name_simple, 'trust_type':trust_type,'companies_house_number':companies_house_number,'companies_house_url':companies_house_url_stub+companies_house_number,'school_count':1})
+			 trust_name_simple=re.sub('[^a-zA-Z0-9 \-\.@]', '', trust_name)
+			 trust_name_simple=trust_name_simple.replace (' ', '-')
+			 trust_name_simple=trust_name_simple.replace ('--', '-')
+			 trust_list.append({
+			 	'trust_code':trust_code,
+				'trust_name':trust_name,
+				'trust_name_simple':trust_name_simple,
+				'trust_type':trust_type,
+				'companies_house_number':companies_house_number,
+				'companies_house_url':companies_house_url_stub+companies_house_number,
+				'school_count':1,
+				'estab_type_count':estab_type_count})
 
-dir=('C:/Users/pn/Documents/Work/Coding/GitHub/ainfo/data')
-os.chdir(dir)
 
-with open('trusts.json', 'w') as out_file:
-    json.dump(trust_list, out_file)
+# dir=('C:/Users/pn/Documents/Work/Coding/GitHub/ainfo/data')
+# os.chdir(dir)
+#
+# with open('trusts.json', 'w') as out_file:
+#	 json.dump(trust_list, out_file)
+
+
+# Sort trust_list
+trust_list=sorted(trust_list, key=lambda k:k['trust_name'])
 
 
 # Create a folder for each trust with a copy of template page
@@ -75,11 +119,7 @@ for trust in trust_list:
 		new_h1.string=trust['trust_name']
 		soup.body.append(new_h1)
 		with open(file_path, 'w') as write_file:
-		    write_file.write(str(soup))
-
-
-# Sort trust_list
-trust_list=sorted(trust_list, key=lambda k:k['trust_name']) 
+			write_file.write(str(soup))
 
 
 # Generate ainfo index.html
@@ -100,4 +140,4 @@ for trust in trust_list:
 		line_break=soup.new_tag('br')
 		soup.body.append(line_break)
 with open('index.html', 'w') as write_file:
-    write_file.write(str(soup))
+	 write_file.write(str(soup))
