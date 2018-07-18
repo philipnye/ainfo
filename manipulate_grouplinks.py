0598ed44034f5a6d8ab9cc9dce55a8c38efbffb3
Philip Nye <contact@philipnye.co.uk>
2018-07-18 07:50:11 +0100

    Set table to defer rendering, add pupil numbers

--- manipulate_grouplinks.py
+++ manipulate_grouplinks.py
@@ -40,50 +40,28 @@ html=requests.get(github_url).text
 soup=BeautifulSoup(html, 'html.parser')
 
 for a in soup.find_all('a'):
-	if str(a.get('title')).endswith('csv') and str(a.get('title')).startswith('grouplinks'):
-		grouplinks_file_name=str(a.get('title'))
-		grouplinks_file_name_date=re.search('[0-9]+', grouplinks_file_name).group()
-		grouplinks_file_name_date=datetime.datetime.strptime(grouplinks_file_name_date, '%Y%m%d').strftime('%d %B %Y').lstrip("0")
-		grouplinks_file_url=github_raw_url+grouplinks_file_name		# expectation is that there is one and only data file
 	if str(a.get('title')).endswith('csv') and str(a.get('title')).startswith('edubasealldata'):
 		edubasealldata_file_name=str(a.get('title'))
 		edubasealldata_file_name_date=re.search('[0-9]+', edubasealldata_file_name).group()
 		edubasealldata_file_name_date=datetime.datetime.strptime(edubasealldata_file_name_date, '%Y%m%d').strftime('%d %B %Y').lstrip("0")
 		edubasealldata_file_url=github_raw_url+edubasealldata_file_name		# expectation is that there is one and only data file
-
-grouplinks_file=requests.get(grouplinks_file_url)
-grouplinks_file=grouplinks_file.iter_lines()	  # is required in order for csv file to be read correctly, without errors caused by new-line characters
-grouplinks_reader=csv.DictReader(grouplinks_file)
+	if str(a.get('title')).endswith('csv') and str(a.get('title')).startswith('grouplinks'):
+		grouplinks_file_name=str(a.get('title'))
+		grouplinks_file_name_date=re.search('[0-9]+', grouplinks_file_name).group()
+		grouplinks_file_name_date=datetime.datetime.strptime(grouplinks_file_name_date, '%Y%m%d').strftime('%d %B %Y').lstrip("0")
+		grouplinks_file_url=github_raw_url+grouplinks_file_name		# expectation is that there is one and only data file
 
 edubasealldata_file=requests.get(edubasealldata_file_url)
 edubasealldata_file=edubasealldata_file.iter_lines()	  # is required in order for csv file to be read correctly, without errors caused by new-line characters
 edubasealldata_reader=csv.DictReader(edubasealldata_file)
 
-# for row in edubasealldata_reader:
-# 	trust_code=row['Trusts (code)']
-# 	trust_name=row['Trusts (name)']
-# 	trust_name=trust_name.replace('\xa0', ' ')		# replace characters that will prevent saving as JSON
-# 	trust_name=trust_name.replace('\x92', '\'')
-# 	trust_name=trust_name.replace('\xc9', 'E')
-# 	trust_pupil_numbers=row['NumberOfPupils']
-# 	if any(trust['trust_code']==trust_code for trust in trust_list)==True:
-# 		for trust in trust_list:
-# 			if trust['trust_code']==trust_code:
-# 				trust['pupil_numbers']=trust_pupil_numbers
-# 				break
-# 	else:
-# 		 trust_name_simple=re.sub('[^a-zA-Z0-9 \-\.@]', '', trust_name)
-# 		 trust_name_simple=trust_name_simple.replace (' ', '-')
-# 		 trust_name_simple=trust_name_simple.replace ('--', '-')
-# 		 trust_list.append({
-# 		 	'trust_code':trust_code,
-# 			'trust_name':trust_name,
-# 			'trust_name_simple':trust_name_simple,
-# 			'pupil_numbers':trust_pupil_numbers,
-# 		})
+grouplinks_file=requests.get(grouplinks_file_url)
+grouplinks_file=grouplinks_file.iter_lines()	  # is required in order for csv file to be read correctly, without errors caused by new-line characters
+grouplinks_reader=csv.DictReader(grouplinks_file)
+
 
 for row in grouplinks_reader:
-	if row['Group Type'] in ('Multi-academy trust','Single-academy trust'):
+	if row['Group Type'].lower() in ('multi-academy trust','single-academy trust'):
 		trust_code=row['Linked UID']
 		trust_name=row['Group Name']
 		trust_name=trust_name.replace('\xa0', ' ')		# replace characters that will prevent saving as JSON
@@ -145,19 +123,37 @@ for row in grouplinks_reader:
 				'companies_house_url':companies_house_url_stub+companies_house_number,
 				'school_count':1,
 				'estab_type_count':estab_type_count,
-				'estab_phase_count':estab_phase_count
+				'estab_phase_count':estab_phase_count,
+				'pupil_numbers':0,
+				'pupil_numbers_schools':0,
+				'no_pupil_numbers_schools':0
 			})
 
+for row in edubasealldata_reader:
+	if row['EstablishmentTypeGroup (name)'].lower() in ('academies','free schools') and row['EstablishmentStatus (name)'].lower() in ('open', 'open, but proposed to close'):
+		trust_code=row['Trusts (code)']
+		school_pupil_numbers=row['NumberOfPupils']
+		for trust in trust_list:
+			if trust['trust_code']==trust_code:
+				if school_pupil_numbers=='':
+					trust['no_pupil_numbers_schools']+=1
+				else:
+					trust['pupil_numbers']+=int(school_pupil_numbers)
+					trust['pupil_numbers_schools']+=1
+				break
+
+for trust in trust_list:
+	if trust['no_pupil_numbers_schools']+trust['pupil_numbers_schools']!=trust['school_count']:
+		print trust
 
 dir=('C:/Users/pn/Documents/Work/Coding/GitHub/ainfo/data')
 os.chdir(dir)
 
+# with open('schools.json', 'w') as out_file:
+# 	 json.dump(schools_list, out_file)
+
 with open('trusts.json', 'w') as out_file:
 	 json.dump(trust_list, out_file)
-
-
-# Sort trust_list
-trust_list=sorted(trust_list, key=lambda k:k['school_count'], reverse=True)
 
 
 # Create a folder for each trust with a copy of template page
@@ -189,52 +185,6 @@ with open('index_template.html') as read_file:
 	html=read_file.read()
 soup=BeautifulSoup(html, 'html.parser')
 soup.find(id='gias_date').append(grouplinks_file_name_date)		# specifying a particular div, rather than using soup.div.append(grouplinks_file_name_date)
-# headers=[
-# 	'Trust',
-# 	'Academies',
-# 	'Primary',
-# 	'Secondary',
-# 	'All-through',
-# 	'Alternative provision',
-# 	'Special',
-# 	'Post-16',
-# 	'Sponsored academies',
-# 	'Converter academies',
-# 	'Free schools',
-# 	'UTCs/studio schools'
-# ]
-# tr=soup.new_tag('tr')
-# soup.find(id='tableContainer').find('thead').append(tr)
-# for header in headers:
-# 	th=soup.new_tag('th', scope="col")
-# 	tr.append(th)
-# 	th.append(header)
-# for trust in trust_list:
-# 	tr=soup.new_tag('tr')
-# 	soup.find(id='tableContainer').find('tbody').append(tr)
-# 	data=[
-# 		trust['trust_name'],
-# 		str(trust['school_count']),
-# 		str(trust['estab_phase_count']['primary']),
-# 		str(trust['estab_phase_count']['secondary']),
-# 		str(trust['estab_phase_count']['all_through']),
-# 		str(trust['estab_phase_count']['alternative_provision']),
-# 		str(trust['estab_phase_count']['special']),
-# 		str(trust['estab_phase_count']['post_16']),
-# 		str(trust['estab_type_count']['sponsored_academy']),
-# 		str(trust['estab_type_count']['converter_academy']),
-# 		str(trust['estab_type_count']['free_school']),
-# 		str(trust['estab_type_count']['utc_studio_school'])
-# 	]
-# 	for d in data:
-# 		td=soup.new_tag('td')
-# 		tr.append(td)
-# 		if d==trust['trust_name'] and d.lower()[0]=='a':
-# 			trust_link=soup.new_tag('a', href=trust_page_url)
-# 			trust_link.string=trust['trust_name']
-# 			td.append(trust_link)
-# 		else:
-# 			td.append(d)
 soup=soup.prettify()
 with open('index.html', 'w') as write_file:
 	 write_file.write(str(soup))
